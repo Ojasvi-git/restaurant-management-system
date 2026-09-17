7ba3f632fadbb78677ad96988a2aba5b09872e27
Ojasvi Pundir <kanhaiyapundir4@gmail.com>
2026-09-17 16:22:13 +0530

    Update CORS configuration to use production frontend URL

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 // -----------------------------
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://restaurant-management-system-eta-ochre.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -48,7 +48,7 @@ setIO(io);
 // -----------------------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://restaurant-management-system-eta-ochre.vercel.app/",
     credentials: true,
   })
 );
