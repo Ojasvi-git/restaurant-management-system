d448f374043bec65f224fbd8da496f9763267bc5
Ojasvi Pundir <kanhaiyapundir4@gmail.com>
2026-09-17 16:04:39 +0530

    Update frontend url using render backend live url

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://restaurant-management-system-yfz8.onrender.com", {
   autoConnect: true,
 
   transports: ["polling", "websocket"],
